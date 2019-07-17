10a57c7ffa2ea775cf7d213c0e7b7c59a76afbb9
lrlna <shestak.irina@gmail.com>
2019-07-17 15:57:00 +0200

    femme::Logger::start() impl, setup for pretty_printing

--- examples/log.rs
+++ examples/log.rs
@@ -1,27 +1,11 @@
-use async_log::{instrument, span};
 use log::info;
 
 fn setup_logger() {
-    let logger = femme::Logger::new();
-    async_log::Logger::wrap(logger, || /* get the task id here */ 0)
-        .start(log::LevelFilter::Trace)
-        .unwrap();
+    femme::Logger::new().start(log::LevelFilter::Trace).unwrap();
 }
 
 fn main() {
     setup_logger();
-
-    span!("new level, depth={}", 1, {
-        let x = "beep";
-        info!("look at this value, x={}", x);
-
-        span!("new level, depth={}", 2, {
-            inner("boop");
-        })
-    })
-}
-
-#[instrument]
-fn inner(y: &str) {
-    info!("another nice value, y={}", y);
+    info!("Hello planet");
+    info!("nori cat!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use log::{Log, Metadata, Record};
+use log::{LevelFilter, Log, Metadata, Record};
 
 #[derive(Debug)]
 pub struct Logger {}
@@ -7,17 +7,30 @@ impl Logger {
     pub fn new() -> Self {
         Self {}
     }
+
+    /// Start logging.
+    pub fn start(self, filter: LevelFilter) -> Result<(), log::SetLoggerError> {
+        let res = log::set_boxed_logger(Box::new(self));
+        if res.is_ok() {
+            log::set_max_level(filter);
+        }
+        res
+    }
 }
 
 impl Log for Logger {
-    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
-        true
+    fn enabled(&self, metadata: &Metadata<'_>) -> bool {
+        metadata.level() <= log::max_level()
     }
 
     fn log(&self, record: &Record<'_>) {
         if self.enabled(record.metadata()) {
-            unimplemented!();
+            pretty_print(record)
         }
     }
     fn flush(&self) {}
+}
+
+fn pretty_print(record: &Record<'_>) {
+    println!("{} - {}", record.level(), record.args());
 }
