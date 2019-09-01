8f5524f2be35470134a258302f331d1cae8c096e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-01 10:47:56 +0200

    add clean logger
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

---
+++ examples/log_clean.rs
@@ -0,0 +1,10 @@
+fn main() {
+    femme::clean::Logger::new()
+        .start(log::LevelFilter::Trace)
+        .unwrap();
+    log::error!("Buffer has to be 16 bytes in length");
+    log::warn!("Unauthorized access attempt on /login");
+    log::info!("Listening on port 8080");
+    log::debug!("Getting String as bson value type");
+    log::trace!("Called public function get_type");
+}
