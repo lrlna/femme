a909231ca453f0c3a1459a363670a740959eb5b9
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-05-18 19:06:20 +0200

    fewer allocations for pretty logger

--- src/pretty.rs
+++ src/pretty.rs
@@ -1,25 +1,19 @@
 //! Pretty print logs.
 
 use console::style;
-use crossbeam_queue::ArrayQueue;
 use log::{kv, Level, LevelFilter, Log, Metadata, Record};
 use std::fmt::Write;
 use std::io;
 
 /// Start logging.
 pub(crate) fn start(level: LevelFilter) {
-    let cap = 100; // store 100 messages max.
-    let logger = Box::new(Logger {
-        queue: ArrayQueue::new(cap),
-    });
+    let logger = Box::new(Logger {});
     log::set_boxed_logger(logger).expect("Could not start logging");
     log::set_max_level(level);
 }
 
 #[derive(Debug)]
-pub(crate) struct Logger {
-    queue: ArrayQueue<String>,
-}
+pub(crate) struct Logger {}
 
 impl Log for Logger {
     fn enabled(&self, metadata: &Metadata<'_>) -> bool {
@@ -27,36 +21,16 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &Record<'_>) {
+        use io::Write;
         if self.enabled(record.metadata()) {
             let mut out = String::new();
             format_src(&mut out, &record);
             write!(out, " {}", &record.args()).unwrap();
             format_kv_pairs(&mut out, &record);
-            dbg!();
-            if let Err(err) = self.queue.push(out) {
-                self.flush();
-                self.queue.push(err.0).expect("Could not write to queue");
-            }
+            writeln!(io::stdout(), "{}", out).unwrap();
         }
     }
-    fn flush(&self) {
-        dbg!();
-        use std::io::Write;
-        let stdout = io::stdout();
-        let mut handle = stdout.lock();
-        for _ in 0..self.queue.len() {
-            handle
-                .write_all(&self.queue.pop().unwrap().as_bytes())
-                .expect("Failed to write to stdout");
-        }
-    }
-}
-
-impl Drop for Logger {
-    fn drop(&mut self) {
-        dbg!();
-        self.flush();
-    }
+    fn flush(&self) {}
 }
 
 fn format_kv_pairs(mut out: &mut String, record: &Record) {
